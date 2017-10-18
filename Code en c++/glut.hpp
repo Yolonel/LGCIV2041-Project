eac11d477456d19e6a1ab4cf73210e7d1a3c8d54
ange.ishimwe@student.uclouvain.be <ange.ishimwe@student.uclouvain.be>
2017-10-18 21:25:21 +0200

    J'ai essayé de coder en c++ mais j'ai une erreur avec la fonction dgev

---
+++ Code en c++/glut.hpp
@@ -0,0 +1,10 @@
+
+// Fonction données
+void stiffnessMatrix( double x1 , double y1 , double x2 , double y2 , double ea , double k[4][4] );
+
+int trussSolver( int N, int B , int M, double *xy , int *ind ,
+ double *f , int *nc , double *vc , double *ea , double *x , double *r );
+
+void postPro( int B , double *xy , int *ind , double *ea , double *x , double *n);
+
+
